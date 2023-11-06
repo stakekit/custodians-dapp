b391be90b5a70f3d2071b5f7e9c5fcd07f66579d
Philip Zudemberg <philip.zdb@gmail.com>
2023-11-06 09:59:13 +0100

    Initial commit

---
+++ src/app/widget/index.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import "@stakekit/widget/package/css"
+import { SKApp, darkTheme } from "@stakekit/widget"
+import { config } from "../../config"
+import mixpanel from "mixpanel-browser"
+import { tracking } from "../tracking"
+
+if (typeof window !== "undefined" && config.mixPanelToken) {
+  mixpanel.init(config.mixPanelToken)
+}
+
+export const Widget = () => {
+  return (
+    <SKApp
+      apiKey={config.apiKey}
+      theme={darkTheme}
+      connectKitForceTheme="darkMode"
+      tracking={tracking}
+    />
+  )
+}
