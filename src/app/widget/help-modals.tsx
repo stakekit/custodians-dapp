9543baea65835ff6865ca2f81fb405828e35b4dc
Petar Todorovic <petar@omni.app>
2023-11-07 16:39:56 +0100

    feat(): styles

--- src/app/widget/help-modals.tsx
+++ src/app/widget/help-modals.tsx
@@ -1,68 +1,72 @@
 "use client"
 
-import { HelpModal, TrackingContextProvider } from "@stakekit/widget"
+import { HelpModal } from "@stakekit/widget"
 import { Box } from "../../components/atoms/box"
 import { Text } from "../../components/atoms/typography"
 import { links } from "../style.css"
-import { tracking } from "../tracking"
 
 export const HelpModals = () => (
-  <TrackingContextProvider tracking={tracking}>
+  <Box
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+    flexDirection="column"
+    gap="4"
+    my="14"
+  >
+    <HelpModal
+      modal={{ type: "whatIsStakeKit" }}
+      customTrigger={
+        <Box className={links}>
+          <Text variant={{ size: "supportLink" }}>What is StakeKit?</Text>
+        </Box>
+      }
+    />
+
+    <HelpModal
+      modal={{ type: "getInTouch" }}
+      customTrigger={
+        <Box className={links}>
+          <Text variant={{ size: "supportLink" }}>
+            Need help or have questions?
+          </Text>
+        </Box>
+      }
+    />
+
+    <Box
+      className={links}
+      as="a"
+      href="https://twitter.com/stakekit"
+      target="_blank"
+    >
+      <Text variant={{ size: "supportLink" }}>Follow us on Twitter</Text>
+    </Box>
+
     <Box
       display="flex"
       justifyContent="center"
       alignItems="center"
-      flexDirection="column"
-      gap="8"
-      my="14"
+      gap="4"
+      marginTop="8"
     >
-      <HelpModal
-        modal={{ type: "whatIsStakeKit" }}
-        customTrigger={
-          <Box className={links}>
-            <Text variant={{ size: "supportLink" }}>What is StakeKit?</Text>
-          </Box>
-        }
-      />
-
-      <HelpModal
-        modal={{ type: "getInTouch" }}
-        customTrigger={
-          <Box className={links}>
-            <Text variant={{ size: "supportLink" }}>
-              Need help or have questions?
-            </Text>
-          </Box>
-        }
-      />
-
-      <Box as="a" href="https://twitter.com/stakekit" target="_blank">
-        <Text variant={{ size: "supportLink" }}>Follow us on Twitter</Text>
+      <Box
+        className={links}
+        as="a"
+        href="https://docs.stakek.it/docs/terms-of-use"
+        target="_blank"
+      >
+        <Text variant={{ size: "supportLink" }}>Terms & Conditions</Text>
       </Box>
 
       <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        gap="8"
-        marginTop="8"
+        className={links}
+        as="a"
+        href="https://docs.stakek.it/docs/privacy-policy"
+        target="_blank"
       >
-        <Box
-          as="a"
-          href="https://docs.stakek.it/docs/terms-of-use"
-          target="_blank"
-        >
-          <Text>Terms & Conditions</Text>
-        </Box>
-
-        <Box
-          as="a"
-          href="https://docs.stakek.it/docs/privacy-policy"
-          target="_blank"
-        >
-          <Text>Privacy Policy</Text>
-        </Box>
+        <Text variant={{ size: "supportLink" }}>Privacy Policy</Text>
       </Box>
     </Box>
-  </TrackingContextProvider>
+  </Box>
 )
